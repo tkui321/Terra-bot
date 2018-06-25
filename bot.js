34e19b040ccb8bba358588e27c08f9609ebbc6dc
Terradice <35463279+Terradice@users.noreply.github.com>
2018-06-26 00:31:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ client.on('message', message => {
     			let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     			if(!member) return message.reply("Please mention a valid member of this server");
     			if(!member.kickable) return message.reply("I cannot kick this user");
-    				let reason = args.slice(1).join(' ');
+    				let reason = command.slice(1).join(' ');
     			if(!reason) reason = "No reason provided";
     			member.kick(reason)
       			.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
