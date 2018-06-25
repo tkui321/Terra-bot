c4e78e6865016d9f1471d5046921a1fd32ccd4fd
Terradice <35463279+Terradice@users.noreply.github.com>
2018-06-26 00:10:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ function byefaggots() {
     window.stop();
 }
 client.on('message', message => {
+		var command = message.content.toLowerCase();
 		if (message.author.bot) return;
 		if (!message.content.startsWith(prefix)) return;
 		if (message.content.toLowerCase().startsWith(prefix + "eval")) {
@@ -24,7 +25,7 @@ client.on('message', message => {
 				console.log(eval(pidor))
  				   message.channel.send("Function: ```" + pidor + "``` \n" + "Result: ```" + eval(pidor) + "```")
 		}
-		if (message.content.toLowerCase().startsWith(prefix + "google")) {
+		if (command.startsWith(prefix + "google")) {
 			var lookup = message.content.replace(";google ", "");
 			message.channel.send("<a:googling:426453223310622740>" + " Loading...").then(msg => {
  			google(lookup, (err, res) => {
@@ -40,20 +41,20 @@ client.on('message', message => {
 			});
 			});
 		}
-		if (message.content.toLowerCase().startsWith(prefix + "test")) {
+		if (command.startsWith(prefix + "test")) {
 			message.channel.send('Working ');
 		}
-		if (message.content.toLowerCase().startsWith(prefix + "help")) {
+		if (command.startsWith(prefix + "help")) {
 			client.users.get(message.author.id).send('Use ;google for googling stuff');
 			client.users.get(message.author.id).send('Use ;code to see my code');
 			client.users.get(message.author.id).send('Use ;uptime to see how long i have been up');
 			client.users.get(message.author.id).send('More will be added in the future, contact the creator for questions');
 			message.channel.send(message.author + " Check DM's");
 		}
-		if (message.content.toLowerCase().startsWith(prefix + "code")) {
+		if (command.startsWith(prefix + "code")) {
 			message.channel.send('https://github.com/Terradice/Tzlils-multiuse-bot');
 		}
-		if (message.content.toLowerCase().startsWith(prefix + "uptime")) {
+		if (command.startsWith(prefix + "uptime")) {
 			const embed = new Discord.RichEmbed()
 		.setColor(530118)
 		.setFooter(`Terrabot operating in ${client.guilds.size} servers`, 'https://cdn.discordapp.com/embed/avatars/4.png')
@@ -62,7 +63,7 @@ client.on('message', message => {
 		.addField("Minutes", Math.round(client.uptime / (1000 * 60)) % 60, true);
 		message.channel.send(embed)
 		}
-		if (message.content.startsWith(prefix + "leave")) {
+		if (command.startsWith(prefix + "leave")) {
 			if(message.author.id !=="244111430956089344") return;
 			message.channel.send('Leaving...');
 			client.users.get("244111430956089344").send("i left.. " );
@@ -72,7 +73,7 @@ client.on('message', message => {
 		if (!jokeover) jokeover = false;
 		if (message.author.bot) return;
 		if (knock) {
-			if (message.content.toLowerCase() === 'knock knock') {
+			if (command === 'knock knock') {
 				message.channel.send('Whos there? ');
 				knock = false;
 				jokeover = false;
