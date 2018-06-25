059a61a807485233058126a0095a79a88ec80670
Terradice <35463279+Terradice@users.noreply.github.com>
2018-06-25 23:33:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,9 +19,9 @@ client.on('message', message => {
 		if (!message.content.startsWith(prefix)) return;
 		if (message.content.toLowerCase().startsWith(prefix + "eval")) {
 			if(message.author.id !== "244111430956089344") return;
-				var snipped = message.content.replace(";eval ", "");
+				var pidor = message.content.replace(";eval ", "");
 				//const pidor= snipped.split(' ').slice(1).join(' ');
-				console.log(eval(snipped))
+				console.log(eval(pidor))
  				   message.channel.send(
 					`\`\`\`js
 					${eval(pidor)}
