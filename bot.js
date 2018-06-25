1560750cbfc41a69b883b5c7ccc6f5faf0bb6f82
Terradice <35463279+Terradice@users.noreply.github.com>
2018-06-26 01:10:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,8 +48,7 @@ client.on('message', message => {
 			let caller = message.guild.members.get(message.author.id);
 			let has_kick = caller.hasPermission("KICK_MEMBERS");
 			console.log(has_kick);
-			if(message.author.has_kick !== message.member.hasPermission("KICK_MEMBERS")) return;
-    			if(message.author.id !== "244111430956089344") return message.reply("Sorry, you don't have permissions to use this!");
+    			if(!has_kick) return message.reply("Sorry, you don't have permissions to use this!");
     			let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     			if(!member) return message.reply("Please mention a valid member of this server");
     			if(!member.kickable) return message.reply("I cannot kick this user");
