bedd362a7d0836fceadab161f368cd0396e168e4
Terradice <35463279+Terradice@users.noreply.github.com>
2018-06-28 00:25:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ client.on('message', message => {
     			if(!reason) reason = "No reason provided";
     			member.kick(reason)
       			.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    			message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    			message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
  		 }
 		if (command.startsWith(prefix + "help")) {
 			var googlehelp = "Use ;google for googling stuff \n";
